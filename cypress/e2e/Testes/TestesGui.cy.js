b92e6a34044fad46997f6d015c55030819d29cdd
Roberta Jesus <roberta@greenlegis.com.br>
2025-03-14 22:17:45 -0300

    Ultimas alterações

--- cypress/e2e/Testes/TestesGui.cy.js
+++ cypress/e2e/Testes/TestesGui.cy.js
@@ -12,8 +12,33 @@ describe("EngageSphere - Interface Inicial API", () => {
       cy.get('[data-testid="size-filter"]')
         .find('option')
         .should('contain', 'All');
-        
+
       });
+
+      // it("CT02 - Retorna à lista de clientes ao clicar no botão Voltar*)", () => {
+       
+    
+      //   });
+
+      //   it("CT03 - Exibie o rodapé com o texto e links corretos *)", () => {
+        
+    
+      //       });
+
+      //     it("CT04 - Exibe a saudação Hi, there quando nenhum nome é fornecido *)", () => {
+        
+    
+      //           });
+
+      //   it("CT05 - Exibe a saudação Hi, Joe quando o nome é fornecido *)", () => {
+        
+    
+      //               });
+
+      // it("CT06 - Exibe o cabeçalho com um título, alternador de tema e um campo de entrada de texto *)", () => {  
+      
+    
+      //                   });
     
   });
   
